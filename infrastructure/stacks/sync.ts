3e085eabe562a370441649c1d7f3ca01cc2326e7
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-04 01:16:03 +0100

    Update

--- infrastructure/stacks/sync.ts
+++ infrastructure/stacks/sync.ts
@@ -1,11 +1,9 @@
-import * as path from "path";
-import fs from "fs";
+import { exec } from "child_process";
 import { Construct } from "constructs";
 import { TerraformStack } from "cdktf";
 import { buildS3Backend } from "@/lib/backends";
 import { buildAWSProvider } from "@/lib/providers";
 import { getS3Bucket } from "@/lib/s3";
-import { S3BucketObject, S3BucketObjectConfig } from "@cdktf/provider-aws/lib/s3";
 import { BUILD_PATH } from "@/config";
 
 export class SyncS3BucketStack extends TerraformStack {
@@ -16,15 +14,12 @@ export class SyncS3BucketStack extends TerraformStack {
     buildAWSProvider(this);
 
     const websiteBucket = getS3Bucket(this);
+    this.sync(`../../${BUILD_PATH}`, websiteBucket.bucket);
+  }
 
-    fs.readdirSync(BUILD_PATH).forEach((file, index) => {
-      new S3BucketObject(this, `bucket-object-${index}-${path.basename(file)}`, <S3BucketObjectConfig>{
-        key: file.replace(BUILD_PATH, ""), 
-        bucket: websiteBucket.bucket,
-        source: path.resolve(file),
-        etag: `${Date.now()}`,
-        contentType: path.extname(file)
-      });
-    });
+  async sync(path: string, bucket: string): Promise<void> {
+    const { stdout, stderr } = await exec(`aws s3 sync ${path} s3://${bucket}`);
+    console.log(stdout);
+    console.log(stderr);
   }
 }
