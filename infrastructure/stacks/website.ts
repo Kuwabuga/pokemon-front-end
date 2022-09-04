36686fb66e8ac0926393632e39a1ca49d71661af
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-04 03:34:20 +0100

    Improvements (#9)

--- infrastructure/stacks/website.ts
+++ infrastructure/stacks/website.ts
@@ -6,7 +6,7 @@ import { createHostedZoneRecord, getHostedZone } from "@/lib/route53";
 import { getHostedZoneCertificate } from "@/lib/acm";
 import { buildBucketPolicy } from "@/lib/iam";
 import { buildWebsiteCloudfrontDistribution, buildCloudfrontOAI, buildRedirectCloudfrontDistribution } from "@/lib/cloudfront";
-import { buildWebsiteBucket, buildRedirectBucket, setS3BucketPolicy, setS3BucketBlockPublicAccess } from "@/lib/s3";
+import { buildWebsiteBucket, buildRedirectBucket, setS3BucketPolicy, setS3BucketBlockPublicAccess, setS3BucketWebsiteConfig } from "@/lib/s3";
 import { AWS_ADMINISTRATIVE_REGION, IS_PRODUCTION, domain, subdomain } from "@/config";
 
 export class WebsiteStack extends TerraformStack {
@@ -28,14 +28,15 @@ export class WebsiteStack extends TerraformStack {
     setS3BucketPolicy(this, undefined, websiteBucket, websitePolicyDocument);
 
     const websiteDistribution = buildWebsiteCloudfrontDistribution(this, undefined, undefined, certificate, websiteBucket, oai);
-    createHostedZoneRecord(this, undefined, `${subdomain}.${domain}`, domainHostedZone, websiteDistribution);
+    const websiteRecord = createHostedZoneRecord(this, undefined, `${subdomain}.${domain}`, domainHostedZone, websiteDistribution);
 
     if (IS_PRODUCTION) {
       // Redirects example.com to www.example.com
       const redirectBucket = buildRedirectBucket(this);
-      setS3BucketBlockPublicAccess(this, "redirect-bucket-public-access", websiteBucket);
+      setS3BucketBlockPublicAccess(this, "redirect-bucket-public-access", websiteBucket, false);
       const redirectBucketPolicyDocument = buildBucketPolicy(this, "default-redirect-bucket-policy-document", websiteBucket, undefined);
       setS3BucketPolicy(this, "redirect-bucket-policy", redirectBucket, redirectBucketPolicyDocument);
+      setS3BucketWebsiteConfig(this, undefined, redirectBucket, websiteRecord);
 
       const redirectDistribution = buildRedirectCloudfrontDistribution(this, undefined, undefined, certificate, redirectBucket);
       createHostedZoneRecord(this, "redirect-route53-record", domain, domainHostedZone, redirectDistribution);
