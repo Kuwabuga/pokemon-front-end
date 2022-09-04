15c68bac543c097c2213b163dbf2d422ed126c76
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-04 03:33:33 +0100

    Improvements

--- infrastructure/lib/s3.ts
+++ infrastructure/lib/s3.ts
@@ -1,7 +1,8 @@
 import { Construct } from "constructs";
 import { DataAwsIamPolicyDocument } from "@cdktf/provider-aws/lib/iam";
-import { DataAwsS3Bucket, S3Bucket, S3BucketConfig, S3BucketPolicy, S3BucketPolicyConfig, S3BucketPublicAccessBlock, S3BucketPublicAccessBlockConfig } from "@cdktf/provider-aws/lib/s3";
+import { DataAwsS3Bucket, S3Bucket, S3BucketConfig, S3BucketPolicy, S3BucketPolicyConfig, S3BucketPublicAccessBlock, S3BucketPublicAccessBlockConfig, S3BucketWebsiteConfiguration, S3BucketWebsiteConfigurationConfig, S3BucketWebsiteConfigurationRedirectAllRequestsTo } from "@cdktf/provider-aws/lib/s3";
 import { DEFAULTS, websiteBucketName, redirectBucketName } from "@/config";
+import { Route53Record } from "@cdktf/provider-aws/lib/route53";
 
 export const buildWebsiteBucket = (scope: Construct, bucketName = websiteBucketName): S3Bucket => {
   return new S3Bucket(scope, `${bucketName}-bucket`, <S3BucketConfig>{
@@ -41,5 +42,20 @@ export const setS3BucketPolicy = (
   return new S3BucketPolicy(scope, id, <S3BucketPolicyConfig>{
     bucket: bucket.id,
     policy: policy.json
+  });
+};
+
+export const setS3BucketWebsiteConfig = (
+  scope: Construct, 
+  id = "default-bucket-website-config", 
+  bucket: S3Bucket | DataAwsS3Bucket,
+  record: Route53Record
+): S3BucketWebsiteConfiguration => {
+  return new S3BucketWebsiteConfiguration(scope, id, <S3BucketWebsiteConfigurationConfig>{
+    bucket: bucket.id,
+    redirectAllRequestsTo: <S3BucketWebsiteConfigurationRedirectAllRequestsTo>{
+      hostName: record.name,
+      protocol: "https"
+    }
   });
 };
