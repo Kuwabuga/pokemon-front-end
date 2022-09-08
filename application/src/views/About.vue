e7e1225292347bbb459bc2d55e96477772c96e13
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-08 02:27:02 +0100

    Improvements

--- application/src/views/About.vue
+++ application/src/views/About.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <h1>Not found!</h1>
+  <h1>About</h1>
 </template>
 
 <style scoped>
--- application/vite.config.ts
+++ application/vite.config.ts
@@ -2,20 +2,30 @@ import { resolve } from "path";
 import { ServerOptions } from "https";
 import vue from "@vitejs/plugin-vue";
 import { AliasOptions, BuildOptions, defineConfig, ResolveOptions, UserConfigExport } from "vite";
+import { DevOptions, ManifestOptions, VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-export default defineConfig(
-  <UserConfigExport>{
-  	plugins: [vue()],
-  	resolve: <ResolveOptions>{
-  		alias: <AliasOptions>{
-  			"@": resolve(__dirname, "./src")
-  		}
-  	},
-  	server: <ServerOptions>{
-  		port: 8040
-  	},
-  	build: <BuildOptions>{
-  		chunkSizeWarningLimit: 600
-  	}
-  }
-);
+const vuePlugin = vue();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const vitePWAPlugin = VitePWA(<Partial<VitePWAOptions>>{
+	registerType: "autoUpdate",
+	manifest: <Partial<ManifestOptions>>{},
+	devOptions: <DevOptions>{
+		enabled: false
+	}
+});
+
+export default defineConfig(<UserConfigExport>{
+	plugins: [vuePlugin],
+	resolve: <ResolveOptions>{
+		alias: <AliasOptions>{
+			"@": resolve(__dirname, "./src")
+		}
+	},
+	server: <ServerOptions>{
+		port: 3000
+	},
+	build: <BuildOptions>{
+		chunkSizeWarningLimit: 600
+	}
+});
