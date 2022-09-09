6b802d9d63ce8487345f84560764fd1e3576db23
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-09 01:32:22 +0100

    Big changes (#56)

--- infrastructure/lib/providers.ts
+++
@@ -1,9 +0,0 @@
-import { Construct } from "constructs";
-import { AwsProvider } from "@cdktf/provider-aws";
-
-export const buildAWSProvider = (scope: Construct, region: string) => {
-  return new AwsProvider(scope, `${region}-provider`, {
-    region: region,
-    alias: region
-  });
-};
--- infrastructure/main.ts
+++
@@ -1,6 +0,0 @@
-import { App } from "cdktf";
-import { WebsiteStack } from "@/stacks/website";
-
-const app = new App();
-new WebsiteStack(app, "website-stack");
-app.synth();
